238379754ed4f13b94b5398f51c5e6634f841ebd
Andrew McKnight <amcknight2718@gmail.com>
2017-03-07 20:20:31 -0500

    copy all scripts

--- Formula/xcbs.rb
+++ Formula/xcbs.rb
@@ -6,8 +6,12 @@ class Xcbs < Formula
   sha256 "b56893f428884cd4ba8d1164de6eb8f85db789e01b910a85f2e11eacf9c33526"
 
   def install
+    bin.install "scripts/list-configurations.rb"
     bin.install "scripts/xcbs"
+    bin.install "scripts/sort-settings-by-values.rb"
+    ln_sf "#{bin}/list-configurations.rb", "/usr/local/bin/list-configurations.rb"
     ln_sf "#{bin}/xcbs", "/usr/local/bin/xcbs"
+    ln_sf "#{bin}/sort-settings-by-values.rb", "/usr/local/bin/sort-settings-by-values.rb"
   end
 
   test do
