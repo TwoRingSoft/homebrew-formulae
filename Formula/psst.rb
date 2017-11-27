e2df43b1e5d14ea83dd4727e52599681d53dad9b
Andrew McKnight <amcknight2718@gmail.com>
2017-11-26 23:06:10 -0700

    update shasum

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -2,7 +2,7 @@ class Psst < Formula
   desc "Insert secrets into codebases that don't want to commit them to history."
   homepage "https://github.com/TwoRingSoft/psst"
   url "https://github.com/TwoRingSoft/psst/archive/1.0.0+1.tar.gz"
-  sha256 "cb577db06ee8236672c1e4e3ca2707a0b8851ca4d35f1b74cf46a23629b3c7d7"
+  sha256 "d3a0fa9bd380884061392476f5742251ccf0de0efd4c8840e8d09a9eeb1fba6a"
   version "1.0.0"
 
   def install
