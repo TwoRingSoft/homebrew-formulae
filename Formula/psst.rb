0b98daaac3240deb0ada7a260463ea6e38f19033
Andrew McKnight <amcknight2718@gmail.com>
2017-11-27 10:16:50 -0700

    update sha for new v1.0.0

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -2,7 +2,7 @@ class Psst < Formula
   desc "Insert secrets into codebases that don't want to commit them to history."
   homepage "https://github.com/TwoRingSoft/psst"
   url "https://github.com/TwoRingSoft/psst/archive/1.0.0+1.tar.gz"
-  sha256 "d3a0fa9bd380884061392476f5742251ccf0de0efd4c8840e8d09a9eeb1fba6a"
+  sha256 "68cbdc8c9318bf63dd6403034ca8388e0944e63b39e0981084e5547aaecb08e6"
   version "1.0.0"
 
   def install
