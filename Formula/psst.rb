db6a63d47341478db5115107a6301442c9588f6e
Andrew McKnight <amcknight2718@gmail.com>
2017-11-26 23:04:30 -0700

    add explicit version

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -3,12 +3,9 @@ class Psst < Formula
   homepage "https://github.com/TwoRingSoft/psst"
   url "https://github.com/TwoRingSoft/psst/archive/1.0.0+1.tar.gz"
   sha256 "cb577db06ee8236672c1e4e3ca2707a0b8851ca4d35f1b74cf46a23629b3c7d7"
+  version "1.0.0"
 
   def install
     prefix.install "psst"
-  end
-
-  test do
-    system "psst", "--help"
   end
 end
