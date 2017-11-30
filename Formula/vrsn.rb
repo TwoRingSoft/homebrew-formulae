d5fe95aba0107b0e4385d645f8ba6105b52e2359
Andrew McKnight <amcknight2718@gmail.com>
2017-11-30 00:26:55 -0700

    update sha

--- Formula/vrsn.rb
+++ Formula/vrsn.rb
@@ -3,7 +3,7 @@ class Vrsn < Formula
   homepage "https://github.com/TwoRingSoft/Vrsnr"
   url "https://github.com/TwoRingSoft/Vrsnr/archive/2.0.1+b15.tar.gz"
   version "2.0.1"
-  sha256 "5eeaf5509cb3e8450bcf27b3f8920058806c995870b6853de72451fb9a0368e5"
+  sha256 "b37896c0613999d85753eeef16b3e9399d77f283d2390844dc064b178a848c14"
 
   depends_on :xcode => "8.2.1"
 
