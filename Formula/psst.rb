7f730eed44382d47fb4e470a8523a36fbfb358f1
Andrew McKnight <amcknight2718@gmail.com>
2018-04-20 21:51:45 -0600

    update psst to have usage output and pass brew audit

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -1,12 +1,15 @@
 class Psst < Formula
   desc "Insert secrets into codebases that don't want to commit them to history"
   homepage "https://github.com/TwoRingSoft/psst"
-  url "https://github.com/TwoRingSoft/psst/archive/1.0.0+1.tar.gz"
-  version "1.0.0"
-  sha256 "68cbdc8c9318bf63dd6403034ca8388e0944e63b39e0981084e5547aaecb08e6"
+  url "https://github.com/TwoRingSoft/psst/archive/1.1.0.tar.gz"
+  sha256 "3f96a89c3e12815f68308db867659511e62682956a3edf2e78f293808ae9039f"
 
   def install
     prefix.install "psst"
     bin.install_symlink "#{prefix}/psst"
+  end
+
+  test do
+    system "#{bin}/psst", "--help"
   end
 end
