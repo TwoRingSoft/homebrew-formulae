6af824f02949b629384eb9ecfedd48201a0eb68a
Andrew McKnight <amcknight2718@gmail.com>
2018-04-20 20:29:28 -0600

    fix brew audit checks; upgrade travis image to xcode 9.3

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -1,9 +1,9 @@
 class Psst < Formula
-  desc "Insert secrets into codebases that don't want to commit them to history."
+  desc "Insert secrets into codebases that don't want to commit them to history"
   homepage "https://github.com/TwoRingSoft/psst"
   url "https://github.com/TwoRingSoft/psst/archive/1.0.0+1.tar.gz"
-  sha256 "68cbdc8c9318bf63dd6403034ca8388e0944e63b39e0981084e5547aaecb08e6"
   version "1.0.0"
+  sha256 "68cbdc8c9318bf63dd6403034ca8388e0944e63b39e0981084e5547aaecb08e6"
 
   def install
     prefix.install "psst"
--- Formula/xcbs.rb
+++ Formula/xcbs.rb
@@ -1,8 +1,8 @@
 class Xcbs < Formula
   desc "Extra checks for your Xcode project"
   homepage "https://github.com/TwoRingSoft/xcbs"
-  url "https://github.com/TwoRingSoft/xcbs/archive/1.0.0+b1.tar.gz"
-  version "1.0.0"
+  url "https://github.com/TwoRingSoft/xcbs/archive/1.0.1.tar.gz"
+  version "1.0.1"
   sha256 "b56893f428884cd4ba8d1164de6eb8f85db789e01b910a85f2e11eacf9c33526"
 
   def install
