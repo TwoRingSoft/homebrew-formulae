37eedea024604262d8d82d127f11690bf9b85704
Andrew McKnight <amcknight2718@gmail.com>
2018-04-20 21:54:34 -0600

    add --online flag to brew audit calls

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -4,4 +4,4 @@ set -eou pipefail
 
 brew tap tworingsoft/formulae
 
-find Formula -name "*.rb" -type f | xargs -I @ basename @ .rb | xargs -t -I @ brew audit --strict tworingsoft/formulae/@
+find Formula -name "*.rb" -type f | xargs -I @ basename @ .rb | xargs -t -I @ brew audit --strict --online tworingsoft/formulae/@
