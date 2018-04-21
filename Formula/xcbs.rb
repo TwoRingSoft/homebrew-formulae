ccdc3d9d0958b9d068cdd7bb936e399ed38c2fa3
Andrew McKnight <amcknight2718@gmail.com>
2018-04-20 20:48:03 -0600

    remove redundant version declaration (brew audit)

--- Formula/xcbs.rb
+++ Formula/xcbs.rb
@@ -2,7 +2,6 @@ class Xcbs < Formula
   desc "Extra checks for your Xcode project"
   homepage "https://github.com/TwoRingSoft/xcbs"
   url "https://github.com/TwoRingSoft/xcbs/archive/1.0.1.tar.gz"
-  version "1.0.1"
   sha256 "1d206749c5986b9d37abc0fe35fe11b8d2e3d2e2d38a8c0daf2f13f48dd5b81e"
 
   def install
