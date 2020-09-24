407ce4d0c9e825db41d41001dcae450ddff57da3
Andrew McKnight <andrew@specto.dev>
2020-09-23 23:24:48 -0800

    add head version of vrsnr

--- Formula/vrsn.rb
+++ Formula/vrsn.rb
@@ -4,6 +4,7 @@ class Vrsn < Formula
   url "https://github.com/TwoRingSoft/Vrsnr/archive/2.1.0.tar.gz"
   version "2.1.0"
   sha256 "084452d232763639e802340c9a0459e430e80485bb050007090580d8e867ef08"
+  head "https://github.com/tworingsoft/Vrsnr.git"
 
   depends_on :xcode => "10.0.0"
 
